0bfe010b831904143358623ffb3b55a207da5e42
Chris Stavitsky <corridorr@gmail.com>
2023-04-07 12:58:47 -0700

    ec 1

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -5,10 +5,17 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const [squares, setSquares] = React.useState(Array(9).fill(null))
+  const [squares, setSquares] = React.useState(
+    () =>
+      JSON.parse(window.localStorage.getItem('squares')) || Array(9).fill(null),
+  )
   const nextValue = calculateNextValue(squares)
   const winner = calculateWinner(squares)
   const status = calculateStatus(winner, squares, nextValue)
+
+  React.useEffect(() => {
+    window.localStorage.setItem('squares', JSON.stringify(squares))
+  }, [squares])
 
   function selectSquare(square) {
     if (squares[square] || winner) {
